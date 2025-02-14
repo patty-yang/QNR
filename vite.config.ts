1be8115afb3622cf1fb79ac6281b6955a78f813c
patty-yang <643017440@qq.com>
2025-02-14 18:46:07 +0800

    feat: 调试服务端

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,6 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
